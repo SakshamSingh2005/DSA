490bf7e9ea7a25e0b16cca5e1e984176f8e556f0
Saksham Singh <sakshamuni1@gmail.com>
2026-04-30 02:17:02 +0530

    Time: 5 ms (97.67%) | Memory: 43.1 MB (42.54%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -4,16 +4,14 @@ class Solution {
         int count = 0;
         
         for(int i = 0; i < n; i++){
-            count += expand(s, i, i);     // odd
-            count += expand(s, i, i + 1); // even
+            count += expand(s, i, i);     
+            count += expand(s, i, i + 1);
         }
         
         return count;
     }
-    
     private int expand(String s, int left, int right){
-        int count = 0;
-        
+        int count = 0;       
         while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
             count++;
             left--;
